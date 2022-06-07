74a2fbf1d5bd95d602c47a940e573ab3bf40fd6a
Katie Dawkins <katiemdawkins@gmail.com>
2022-06-07 11:10:43 -0500

    initial set up

---
+++ swole/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+# from swole.views import register_user, login_user
+# from rest_framework import routers
+
+
+# router = routers.DefaultRouter(trailing_slash=False)
+# router.register(r'gametypes', GameTypeView, 'gametype')
+# router.register(r'events', EventView, 'event')
+# router.register(r'games', GameView, 'game')
+
+
+urlpatterns = [
+    # path('register', register_user),
+    # path('login', login_user),
+    # path('admin/', admin.site.urls),
+    # path('', include(router.urls)),
+
+]
