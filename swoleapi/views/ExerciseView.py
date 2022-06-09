ed490ba6491d856fea22d23ac6be811b54522402
Katie Dawkins <katiemdawkins@gmail.com>
2022-06-09 09:24:43 -0500

    filters added to exercise view

--- swoleapi/views/ExerciseView.py
+++ swoleapi/views/ExerciseView.py
@@ -20,11 +20,27 @@ class ExerciseView(ViewSet):
             return Response (serializer.data)
         except Exercise.DoesNotExist as ex:
             return Response ({'message':ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-        
+
+
+#################add filters to list view tomorrow!!!!!!!!!!
     def list(self, request):
         """Handel Get Requests to get all exercises"""
         
         exercises = Exercise.objects.all()
+        
+        name = request.query_params.get('name', None)
+        category = request.query_params.get('category', None)
+        body_part = request.query_params.get('body_part', None)
+        
+        if name is not None:
+            exercises = exercises.filter(name__icontains=name)
+        
+        if category is not None:
+            exercises = exercises.filter(category__id=category)
+            
+        if body_part is not None:
+            exercises = exercises.filter(body_part__id=body_part)
+                
         serializer = ExerciseSerializer(exercises, many=True)
         return Response(serializer.data)
     
