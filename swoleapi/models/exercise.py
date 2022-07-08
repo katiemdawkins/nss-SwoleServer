0346ae2c76efd879a11605f893de443c090d9b9c
Katie Dawkins <katiemdawkins@gmail.com>
2022-07-08 11:47:34 -0500

    userr can create a new exercise

--- swoleapi/models/exercise.py
+++ swoleapi/models/exercise.py
@@ -7,3 +7,13 @@ class Exercise(models.Model):
     image_url = models.CharField(max_length=200)
     body_part = models.ForeignKey("Body_Part", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    
+    
+    #custom property adding user id for create exercise
+    @property
+    def current_user(self):
+        return self.__current_user
+    
+    @current_user.setter
+    def current_user(self,value):
+        self.__current_user=value
